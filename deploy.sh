4788dca49bf0b702247f2a081f7b4af583a4a512
Greg Hale <imalsogreg@gmail.com>
2016-01-07 16:45:57 -0500

    fix up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 .cabal-sandbox/bin/site clean
+git submodule init
+git submodule update
+cd _site && git checkout master && cd ..
 .cabal-sandbox/bin/site build
-cp README.md _site/
-cd _site && git add *.html *.png *.jpg *.gif *.js *.css *.md
-cd _site && git commit -m "build" && git push origin master
+cp README.md _site/README.md
+cd _site && git add -A && git commit -m "build" && git push origin master
